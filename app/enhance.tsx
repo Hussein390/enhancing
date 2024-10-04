33d20160a235467d3d95bec4cd0cb5df23f0c7ee
Your Name <hsssseeeen2002@gmail.com>
2024-10-04 14:38:18 +0300

    add something

--- app/enhance.tsx
+++ app/enhance.tsx
@@ -101,6 +101,7 @@ export default function Enhance({ years }: Enhances) {
   return (
     <div className="relative w-[380px] mx-auto p-3 select-none">
       <h1 className="text-4xl  text-center font-semibold">This is <span className="text-blue-500">{new Date().getFullYear()}</span> Dude</h1>
+      <p className="text-slate-400 text-center mt-1">Do not wish it was easier, wish you were better </p>
       <p className="text-slate-400 text-center mt-1">It is time to enhance, does not it!</p>
 
       <div className=" w-[200px] mx-auto mt-4">
